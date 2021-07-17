74b4f5926a35c96c56a8999804ec9651fb354b39
myabarca <66966794+myabarca@users.noreply.github.com>
2021-07-16 17:31:33 -0700

    loan and reference pages

---
+++ loan-search.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+<title>CASG Loan Search</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+</head>
+
+
+<body>
+
+		<nav class="navbar navbar-default">
+		<div class="container-fluid">
+		<a href="index.php" class="navbar-brand">Back to Home</a> 
+		</div>
+
+		<div class="container d-flex align-items-center">
+		
+			<div class="container p-3 my-3 bg-primary text-white"> 
+			<h1>CASG Loan Search</h1>
+			</div>
+			<p>Under construction. Please check back later.</p>
+		
+		</div>
